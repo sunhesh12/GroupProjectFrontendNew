fa6199c4a2264b7d0a957aac6cf56b902e7ec9ed
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2025-06-16 18:19:52 +0530

    Seperately fetching teacherr and student modules

--- src/app/app/modules/page.tsx
+++ src/app/app/modules/page.tsx
@@ -1,76 +1,74 @@
 import style from "./page.module.css";
 import SearchBar from "./search-bar";
-import SemesterSelector from "./semester-selector";
-import CourseList from "./module-list";
-import EnrollmentModal from "./enrollment-modal";
-import type { ModuleWithCourses } from "@/utils/types/backend";
 import { redirect } from "next/navigation";
-import { courses } from "@/utils/backend";
 import { user } from "@/utils/backend";
+import Link from "next/link";
+import { getSession } from "@/utils/auth";
+import ModuleCard from "@/components/module-card/module-card";
 
-export default async function Modules({
-  searchParams,
-}: {
-  modules: ModuleWithCourses[];
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+export default async function Modules() {
+  const session = await getSession();
+  console.log(session);
 
-  // if (!session?.user) {
-  //   // If un authenticated redirected to signin
+  if (!session) {
+    // If unauthenticated, redirect to signin
+    redirect("/auth/signin");
+  }
+
+  const userResponse = await user.get(session);
+
+  if (userResponse.status === 404 || !userResponse.payload) {
+    redirect("/app/auth/signin");
+  }
+
+  if (userResponse.status === 500) {
+    throw new Error("Internal server error while fetching the user");
+  }
+
+  const modules =
+    userResponse.payload.role === "student"
+      ? await user.modules(session)
+      : await user.getTeachingModules(session);
+
+  if (!modules.payload) {
+    return;
+  }
+
+  // if(!session.payload?.user || !session.payload?.token) {
+  //   // Empty user returned
   //   redirect("/auth/signin");
   // }
-
-  // if (!session) {
-  //   // If un authenticated redirected to signin
-  //   redirect("/auth/signin");
-  // }
-
-  // if (!session.user.id) {
-  //   throw new Error(
-  //     "Malformed session !, User ID is not available in the session"
-  //   );
-  // }
-
-  //const currentUser = await user.get("11");
-
-  // const modules =
-  //   currentUser.payload.Role === "lecturer"
-  //     ? (await user.getTeachingModules(currentUser.payload.id)).payload
-  //     : (await courses.getModules(currentUser.payload.id)).payload;
-
-  // Extract search query and selected semester from URL search params
-  const params = (await searchParams) as {
-    searchQuery: string;
-    selectedSemester: string;
-  };
-  console.log(params.searchQuery);
 
   return (
     <main className={style.mainWrapper}>
       <header className={style.courseHeader}>
         <h1 className={style.heading}>Modules</h1>
-        {/* {user.courseId ? (
-          <p className={style.subHeading}>
-            List of all the courses available throughout the degree programme.
-          </p>
-        ) : (
-          <p className={style.subHeading}>
-            You might not have assigned to any course.
-          </p>
-        )} */}
-
         <div className={style.filterCourse}>
-          <SearchBar
-            searchQuery={params.searchQuery}
-            semesters={["1", "2", "3"]}
-          />
+          <SearchBar searchQuery={""} semesters={["1", "2", "3"]} />
         </div>
       </header>
-      {modules ? (
-        <CourseList filteredModules={modules} />
-      ) : (
-        <p>No modules available for this course</p>
-      )}
+      <div className={style.courseBody}>
+        {modules.payload.length > 0 ? (
+          modules.payload.map((module) => (
+            <div key={module.id} className={style.cardWrapper}>
+              <Link
+                href={`/app/modules/${module.id}`}
+                className={style.cardLink}
+              >
+                <ModuleCard
+                  id={module.id}
+                  imageUrl={module.image ?? "/module-cover.webp"}
+                  completion={0}
+                  name={module.module_name}
+                  semester={Number(module.semester)}
+                />
+              </Link>
+            </div>
+          ))
+        ) : (
+          <p>No modules assigned for this course</p>
+        )}
+      </div>
     </main>
   );
 }
